3f1cf98f3fa136baf52bfe4a7b321c1b90441272
Sven Weidauer <sven.weidauer@gmail.com>
2012-01-07 20:37:46 +0100

    Initial Commit

---
+++ LLVMForth/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LLVMForth
+//
+//  Created by Sven Weidauer on 07.01.12.
+//  Copyright (c) 2012 Bright Solutions. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
